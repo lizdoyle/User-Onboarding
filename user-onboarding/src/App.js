dcf94836f36f243441c41c06f50f09d52eafe8db
lizdoyle <lizdoyle1121@gmail.com>
2019-08-16 03:53:55 -0400

    start of advnc st mngmnt/ed

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import Form from './components/Form';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Form = () => {
+
+
+  return (
+    <div> </div>
+
+
+  );
+
+};
+
+export default Form
